3866e0b53c395a24118fce17714fd289a860d38b
WillHDev <will.v.holcombe@gmail.com>
2024-08-24 18:25:52 -0500

    checkout test working

--- tests/checkout.spec.js
+++ tests/checkout.spec.js
@@ -1,25 +1,80 @@
-// const {test, expect} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 
+test.beforeEach(async ({ page }) => {
 
-// test('Complete checkout successfully', async({page}) =>{
+    //sign in
+    await page.goto('https://www.saucedemo.com/');
+  
+    // await page.getByRole('input', { name: 'username', exact: true }).click();
+    await page.getByPlaceholder('Username').click();
+    await page.getByPlaceholder('Username').fill('standard_user');
 
+    await page.getByPlaceholder('Password').fill('secret_sauce');
 
-//     await page.getByRole( 'button', { name: 'Checkout' }).click();
+    await page.locator("[type=submit]").click();
 
-//     //fill out shipping information form
+    //await page.locator('button:text("Login")').click();
 
-//     First Name
-// Last Name
-// Zip/Postal Code
+    await expect(page).toHaveTitle('Swag Labs');
 
-//         //MENTOR:is it best practices to inlcude this line?  Is there a cleaner way to write it?
-//         await expect(page.getByText('Sauce Labs Fleece Jacket')).toBeVisible();
+   
+
+    await page.waitForTimeout(5000);
+
+    //add item to cart
+    await page.locator('.inventory_item_description')
+.filter({ hasText: 'Sauce Labs Fleece Jacket' })
+    .getByRole('button', { name: 'Add to cart' })
+    .click();
+
+    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+
     
+    await page.locator('.shopping_cart_badge').click();
+    //assert Fleece is in cart
+
+    const inventoryItem = await page.locator('.inventory_item_name');
+    await expect(inventoryItem).toHaveText('Sauce Labs Fleece Jacket');
+
+    //MENTOR:is it best practices to inlcude this line?  Is there a cleaner way to write it?
+    await expect(page.getByText('Sauce Labs Fleece Jacket')).toBeVisible();
+
+
+    //assert no other items in cart
+
+    await expect(inventoryItem).toHaveCount(1);
+
+  });
+  
+
+  //zip as a string of integer?
+
+  const SHIPPING_USER_INFO = {
+    firstName: 'Charles',
+    lastName: 'Waterston',
+    zipCode: '55417'
+  };
+
+test('Complete checkout successfully', async({page}) =>{
+
+    const { firstName, lastName, zipCode } = SHIPPING_USER_INFO
+
+    await page.getByRole( 'button', { name: 'Checkout' }).click();
+
+    //fill out shipping information form
+
+    await page.getByPlaceholder('First Name').fill(firstName);
+    await page.getByPlaceholder('Last Name').fill(lastName);
+    await page.getByPlaceholder('Zip/Postal Code').fill(zipCode);
+    await page.getByRole('button', { name: 'Continue' })
+    .click();
+
+    await page.getByRole('button', { name: 'Finish' })
+    .click();
     
-//         //assert no other items in cart
+    await expect(page.getByText('Thank you for your order!')).toBeVisible();
+        
     
-//         await expect(inventoryItem).toHaveCount(1);
-    
-//     });
+    });
     
